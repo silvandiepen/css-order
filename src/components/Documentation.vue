f9b2896feb1275b20ef0f76bf1b5c3459f4cb79d
Sil van Diepen <me@silvandiepen.com>
2020-01-20 13:42:34 +0100

    breaking on console log...

--- src/components/Documentation.vue
+++ src/components/Documentation.vue
@@ -4,30 +4,23 @@
     </section>
 </template>
 <script>
-// import vueMarkdown from 'vue-markdown'
 import Documentation from 'html-loader!markdown-loader?modules!@/assets/data/documentation.md'
 
 export default {
     data: () => ({
         documentation: Documentation,
     }),
-    components: {
-        // vueMarkdown,
-    },
-    created() {
-        console.log(this.documentation)
-    },
 }
 </script>
 
 <style lang="scss">
 pre {
-	background-color: color(_YellowLight);
-	color: color(_Black);
-	code {
-		display: block;
-		line-height: 1.5;
-		padding: 1rem;
-	}
+    background-color: color(_YellowLight);
+    color: color(_Black);
+    code {
+        display: block;
+        line-height: 1.5;
+        padding: 1rem;
+    }
 }
 </style>
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -21,8 +21,8 @@
 
 <style lang="scss">
 .content {
-	a:not([class]) {
-		color: color(SkyBlue);
-	}
+    a:not([class]) {
+        color: color(Skyblue);
+    }
 }
 </style>
