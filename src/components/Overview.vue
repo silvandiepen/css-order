106468b3aed0baa4d6514fd62d64566d85345420
Sil van Diepen <me@silvandiepen.com>
2020-01-20 13:58:24 +0100

    it needs a length in order to work

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -4,10 +4,11 @@
             class="selectors"
             :class="[
                 {
-                    'selectors--searching': search,
+                    'selectors--searching': search && search.length,
                 },
                 {
-                    'selectors--has-properties': value && value.value,
+                    'selectors--has-properties':
+                        value && value.value && value.value.length,
                 },
             ]"
             v-for="(value, index) in filteredCSS"
@@ -92,49 +93,49 @@ export default {
 
 <style lang="scss">
 body {
-	counter-reset: selectors;
+    counter-reset: selectors;
 }
 .selectors {
-	display: flex;
-	flex-direction: row;
-	color: white;
-	transition: opacity $base-transition;
-	&--searching {
-		opacity: 0.25;
-		&.selectors--has-properties {
-			opacity: 1;
-		}
-	}
+    display: flex;
+    flex-direction: row;
+    color: white;
+    transition: opacity $base-transition;
+    &--searching {
+        opacity: 0.25;
+        &.selectors--has-properties {
+            opacity: 1;
+        }
+    }
 
-	&__overview {
-		padding: 2rem;
-		background-color: color(Plum);
-	}
-	&__title {
-		display: block;
-		width: grid(5);
-		font-size: 0.8rem;
-		line-height: 1rem;
-		letter-spacing: 1px;
-		text-transform: uppercase;
-		margin: 0;
-		padding: 0.5em;
-	}
-	&__list {
-		padding: 0;
-		margin: 0;
-	}
-	&__item {
-		display: block;
-		line-height: 1rem;
-		counter-increment: selectors;
-		padding: 0.5em;
-		&::before {
-			content: counter(selectors);
-			display: inline-block;
-			width: 2rem;
-			opacity: 0.25;
-		}
-	}
+    &__overview {
+        padding: 2rem;
+        background-color: color(Plum);
+    }
+    &__title {
+        display: block;
+        width: grid(5);
+        font-size: 0.8rem;
+        line-height: 1rem;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+        margin: 0;
+        padding: 0.5em;
+    }
+    &__list {
+        padding: 0;
+        margin: 0;
+    }
+    &__item {
+        display: block;
+        line-height: 1rem;
+        counter-increment: selectors;
+        padding: 0.5em;
+        &::before {
+            content: counter(selectors);
+            display: inline-block;
+            width: 2rem;
+            opacity: 0.25;
+        }
+    }
 }
 </style>
